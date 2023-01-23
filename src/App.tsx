ab8ca06eb55d1014c0cb80a691a41cb57fa562fd
Eskil Forsell <eskil.forsell@gmail.com>
2023-01-23 23:45:16 +0100

    Install Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import * as ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import * as React from 'react';
+import { Card, CardContent, Container, Typography } from '@mui/material';
+import TopNav from "./AppBar";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
-ReactDOM.render(
-    React.createElement("h2", null, "Hello, world!"),
-    document.body
-);
+const container = document.getElementById("app");
+
+if (container) {
+    const root = createRoot(container);
+
+    root.render(
+        <Grid2 container spacing={3}>
+            <TopNav />
+            <Grid2 xs={12}>
+            
+
+            <Typography variant="h1">Online Experimentation Library</Typography>
+
+            <Card>
+                <CardContent>
+                    <Typography gutterBottom color="text.secondary">Hej svejs</Typography>
+                    <Typography variant="body2">well meaning and kindly.
+                        "a benevolent smile"</Typography>
+                </CardContent>
+            </Card>
+            </Grid2>
+        </Grid2>
+    );
+}
